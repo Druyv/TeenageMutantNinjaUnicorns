64ef44d7a23d70b46c47f5e2aafc30c9c9b2ab28
leojenns <leo.jenns.s2@gmail.com>
2017-02-01 14:41:12 +0100

    because ist easy

--- game/game/main.cpp
+++ game/game/main.cpp
@@ -12,68 +12,75 @@
 #include "npc.hpp"
 #include "menu.hpp"
 #include "button.hpp"
+#include "management.hpp"
+
 
 int main(int argc, const char **argv) {
 	collisions the_collisions;
-	object_ptr dummy = nullptr;
 	// Create the main window
 	sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "The unicorn game", sf::Style::Fullscreen);
-	int menu_item_pressed = -1;
-	menu start_menu(window, "forest.png", true, "CONTINUE", true, "NEW GAME", true, "QUIT");
-
-	actions menu_actions = { action(sf::Mouse::Button::Left, [&]() {menu_item_pressed = start_menu.select(sf::Mouse::getPosition(window)); }) };
 
 	actions unicorn_actions = {
-		action(sf::Keyboard::Left, [](object_ptr object) {object->move(sf::Vector2f(-10.0,0.0)); }),
+		action(sf::Keyboard::A, [](object_ptr object) {object->move(sf::Vector2f(-10.0,0.0)); }),
+		action(sf::Keyboard::D, [](object_ptr object) {object->move(sf::Vector2f(10.0,0.0)); }),
+		action(sf::Keyboard::Space, [](object_ptr object) {object->jump(); }),
+                action(sf::Keyboard::Left, [](object_ptr object) {object->move(sf::Vector2f(-10.0,0.0)); }),
 		action(sf::Keyboard::Right, [](object_ptr object) {object->move(sf::Vector2f(10.0,0.0)); }),
-		action(sf::Keyboard::Space, [](object_ptr object) {object->jump(); })
 	};
-	std::cout << "Menu made!" << std::endl;
-	std::shared_ptr<unicorn> the_unicorn;
-	objects_vector objects;
-	factory object_creation("level1");
 
-	//Making mobs (should be in factory with next level
+	//Menu and file management
+	file_management manager_file("played_games/save_files_path.txt");
+	menu_management manager_menu(window, manager_file);
+	std::string level_path = manager_menu.start_game();
+	std::cout << "Path for level" << level_path;
+
+
+	std::shared_ptr<unicorn> the_unicorn;
+    std::shared_ptr<image_from_file> end_point;
+	objects_vector objects;
+    objects_vector objects_tmp;
+	std::shared_ptr<factory> object_creation = std::make_shared<factory>(level_path);
 	std::vector<mob_ptr> all_mobs;
-	all_mobs.push_back(std::make_shared<mob>(sf::Vector2f(2541.799316f, 1890.0f), "mini_bunny.png"));
 
 
 	try {
-            objects = object_creation.objects_from_file();
+            objects_tmp = object_creation->objects_from_file();
             
-            base_level base( object_creation.get_level_size() );
+            base_level base( object_creation->get_level_size() );
             base.push_back_borders(objects);
             
-            the_unicorn = std::make_shared<unicorn>(object_creation.get_spawn(), "spreadsheet.png", unicorn_actions, the_collisions, all_mobs, objects);
+            the_unicorn = std::make_shared<unicorn>(object_creation->get_spawn(), "spreadsheet.png","rainbowspreadsheet.png", unicorn_actions, the_collisions, all_mobs, objects,manager_file.get_counter());
 		
 	}
 	catch (const std::exception & e) {
             std::cout << e.what();
             exit(0);
 	}
+        
+        
+        for(const auto & object : objects_tmp){
+            if (object->get_type() == "MOB"){
+                all_mobs.push_back( std::make_shared<mob>(object->get_position(), "mini_bunny.png") );
+            }
+            else if(object->get_type() == "END"){
+                end_point = std::make_shared<image_from_file>(object->get_position(), object->get_image_name() );
+                end_point->set_type("END");
+            }
+            else{
+                objects.push_back(object);
+            }
+        }
 
-
+        //delete objects_tmp;
+        
 	//Sound, camera, background
-	background background_1("background.png", object_creation.get_level_size());
+	background background_1("background2.png", object_creation->get_level_size());
 	camera playercam(the_unicorn);
 	soundtrack soundplayer("Soundtrack.wav");
 	soundplayer.PlayMusic();
-
-	//Drawing menu
-	start_menu.build_menu();
-	window.display();
+        
 	// Start the game loop
 	while (window.isOpen()) {
-		while (menu_item_pressed == -1) {
-			for (auto & action : menu_actions) {
-				action();
-			}
-		}
-		window.clear();
-		if (menu_item_pressed == 0 || menu_item_pressed == 2) {
-			std::cout << "Menu works" << std::endl;
-		}
-
 		the_collisions.clear();
 		// Clear screen
 		window.clear();
@@ -83,28 +90,79 @@ int main(int argc, const char **argv) {
 			the_unicorn->collapse(object, the_collisions);
 			object->draw(window);
 		}
-		for (auto objects : all_mobs) {
+		for (auto object : all_mobs) {
 			//std::cout << "Drawing mobs" << std::endl;
-			objects->draw(window);
-			if (the_unicorn->getGlobalBounds().intersects(objects->getGlobalBounds())) {
-				the_unicorn->damage();
+			object->draw(window);
+			if (the_unicorn->getGlobalBounds().intersects(object->getGlobalBounds())) {
+				the_unicorn->damage(object);
 			}
 		}
+                
+            end_point->draw(window);
+            the_unicorn->run_actions(the_unicorn);
+            the_unicorn->draw(window);
 
-		the_unicorn->run_actions(the_unicorn);
-		the_unicorn->draw(window);
+            playercam.follow(window);
 
-		playercam.follow(window);
+            // Update the window
+            window.display();
+            
+            if(the_unicorn->getGlobalBounds().intersects(end_point->getGlobalBounds() )){
+                if (manager_file.get_counter() == 6){
+                    std::cout << "You won!!\n";
+                    auto won = image_from_file( sf::Vector2f{0,0}, "end.png" );
+                    
+                    won.set_scale(0.65,0.65 );
+                    
+                    sf::Vector2f new_position {the_unicorn->get_position().x - (won.get_size().x/2),
+                                                the_unicorn->get_position().y - (won.get_size().y/2)};
+                    std::cout << "scale x: " << (float)sf::VideoMode::getDesktopMode().width / 1920 << '\n';
+                    won.set_position(new_position);
+                    
+                    won.draw(window);
+                    window.display();
+                    sf::sleep(sf::milliseconds(3000));
+                    exit(0);
+                }
+                try {
+                    objects.clear();
+                    objects_tmp.clear();
+                    all_mobs.clear();
+                    object_creation->change_input_to(manager_file.next_level());
+                    objects_tmp = object_creation->objects_from_file();
+                    base_level base( object_creation->get_level_size() );
+                    base.push_back_borders(objects);
+                    the_unicorn->set_spawn_location( object_creation->get_spawn() );
+                }
+                catch (const std::exception & e) {
+                    std::cout << e.what();
+                    exit(0);
+                }
 
-		// Update the window
-		window.display();
-
+                for(const auto & object : objects_tmp){
+                    if (object->get_type() == "MOB"){
+                        all_mobs.push_back( std::make_shared<mob>(object->get_position(), "mini_bunny.png") );
+                    }
+                    else if(object->get_type() == "END"){
+                        end_point = std::make_shared<image_from_file>(object->get_position(), object->get_image_name() );
+                        end_point->set_type("END");
+                    }
+                    else{
+                        objects.push_back(object);
+                    }
+                }
+            }
+            
+            //pauze menu ding
+            if( sf::Keyboard::isKeyPressed(sf::Keyboard::Escape) ){
+                manager_menu.display_pause_game();
+            }
 
 		// Process events
 		sf::Event event;
 		while (window.pollEvent(event)) {
 			// Close window: exit
-			if (event.type == sf::Event::Closed || event.key.code == sf::Keyboard::Escape) {
+			if (event.type == sf::Event::Closed) {// || event.key.code == sf::Keyboard::Escape
 				window.close();
 			}
 		}
